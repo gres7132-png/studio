b362de2b54430cde79c168217b6cb44057e1bbf2
gres <gres7132@gmail.com>
2025-08-27 08:50:23 +0000

    Make sure users cannot access Admin panel except using the admins lag in

--- src/app/(authed)/admin/layout.tsx
+++ src/app/(authed)/admin/layout.tsx
@@ -1,7 +1,6 @@
+
 import { mockUser } from "@/lib/data";
 import { redirect } from "next/navigation";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { ShieldAlert } from "lucide-react";
 
 export default function AdminLayout({
     children,
@@ -11,17 +10,7 @@ export default function AdminLayout({
     const user = mockUser;
 
     if (!user.isAdmin) {
-       return (
-        <div className="p-4">
-             <Alert variant="destructive">
-                <ShieldAlert className="h-4 w-4" />
-                <AlertTitle>Access Denied</AlertTitle>
-                <AlertDescription>
-                    You do not have permission to view this page.
-                </AlertDescription>
-             </Alert>
-        </div>
-       )
+       redirect('/admin/login');
     }
 
     return (
