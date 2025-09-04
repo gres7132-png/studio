169c9eab884afc6dc3220af6c97f6f251cd82ed0
gres <gres7132@gmail.com>
2025-09-04 09:17:05 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
@@ -7,12 +7,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyDnFrJWS2t_05w8i3rLen8UXK6nP8eNY1g",
   authDomain: "fundflow-wzhal.firebaseapp.com",
   projectId: "fundflow-wzhal",
-  storageBucket: "fundflow-wzhal.appspot.com",
+  storageBucket: "fundflow-wzhal.firebasestorage.app",
   messagingSenderId: "231100893920",
   appId: "1:231100893920:web:c6e5fb9a41984b4f85de85"
 };
 
-// Initialize Firebase
-let firebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+// Initialize Firebase for SSR
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-export const auth = getAuth(firebaseApp);
+export const auth = getAuth(app);
