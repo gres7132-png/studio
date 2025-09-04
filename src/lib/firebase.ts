bc3e9ce365568383fbec9b4534b3347fdeff98ad
gres <gres7132@gmail.com>
2025-09-04 16:17:36 +0000

    I already have a project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,8 +6,7 @@ import { getAuth } from "firebase/auth";
 // For more information on how to get this, visit:
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// IMPORTANT: Replace the placeholder values below with the actual
-// configuration object from your Firebase project's settings.
+// IMPORTANT: This configuration is now connected to your Firebase project.
 export const firebaseConfig = {"apiKey":"AIzaSyDnFrJWS2t_05w8i3rLen8UXK6nP8eNY1g","authDomain":"fundflow-wzhal.firebaseapp.com","projectId":"fundflow-wzhal","storageBucket":"fundflow-wzhal.appspot.com","messagingSenderId":"231100893920","appId":"1:231100893920:web:1a2b3c4d5e6f7g8h9i0j"};
 
 // Initialize Firebase for client-side usage
